da7e9476448bbae814304c773ff580ca33fb7d87
Luiz F. Picolo <luizpicolo@gmail.com>
2022-10-06 23:57:27 -0400

    Removido dominios por porta

--- main.py
+++ main.py
@@ -3,26 +3,14 @@ from datetime import datetime
 
 app = Flask(__name__, template_folder='views')
 
-base_url = 'http://sistemas.na.ifms.edu.br'
-
 paths = [
-  ['/sigoe', 'Nova Andradina', '2697'],
-  ['/sigoe', 'Naviraí',        '2696'],
-  ['/sigoe', 'Ponta Porã',     '2693'],
-  ['/sigoe', 'Três Lagoas',    '2692'],
-  ['/sigoe', 'Aquidauana',     '2691'],
-  ['/sigoe', 'Corumbá',        '2690'],
-  ['/sigoe', 'Dourados',       '2689'],
-  ['/sigoe', 'Coxim',          '2688'],
-  ['/sigoe', 'Campo Grande',   '8888'],
-  ['/sigoe', 'Jardim',         '8888'],
+  ['https://sigoe.na.ifms.edu.br', 'Sistema de Ocorrências', '443'],
 ]
 
 @app.route('/')
 def main():
     return render_template(
       'layout.html', 
-      base_url=base_url, 
       paths=paths,
       date_now=date_now()
     )
